a19537e92cbddc2e7dec17c193b3cdefc4e2ea7b
Min RK <benjaminrk@gmail.com>
2019-10-22 10:56:31 +0200

    make sure xalloc.h is installed

--- conda/recipes/singular/build.sh
+++ conda/recipes/singular/build.sh
@@ -4,3 +4,6 @@ set -ex
 ./configure --disable-omalloc --with-flint=$PREFIX --prefix=$PREFIX
 make -j${CPU_COUNT}
 make install
+
+# singular fails to install omalloc/xalloc.h, required for downstream builds
+cp omalloc/xalloc.h $PREFIX/include/omalloc/xalloc.h
